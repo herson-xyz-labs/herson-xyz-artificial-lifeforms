b1bb6646aa85dd6a5402da0a7c1e6d1e9ca5d68f
Herson Guerrero Huh <83830469+herson-xyz@users.noreply.github.com>
2024-06-01 15:36:54 -0400

    heroku deployment issues

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default {
     server:
     {
         host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+        open: !(process.env.DYNO || 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
     build:
     {
